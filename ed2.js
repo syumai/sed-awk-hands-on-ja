235afdce51e3fe98217a53370665e96bb8c53f95
syumai <syumai@gmail.com>
2020-12-10 20:00:35 +0900

    Add awk

---
+++ ed2.js
@@ -0,0 +1,7 @@
+function hello() {
+  gonsole.log("Hello, world!"); gonsole.log("Hello, ed!");
+  gonsole.log("Hello, world!"); gonsole.log("Hello, ed!");
+  gonsole.log("Hello, world!"); gonsole.log("Hello, ed!");
+  gonsole.log("Hello, world!"); gonsole.log("Hello, ed!");
+}
+hollo();
